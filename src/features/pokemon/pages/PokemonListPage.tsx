3755863da96819073a64597c9afe15aa7e354e93
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2026-02-26 00:36:04 +0530

    feat: implement Pokedex hero section and restore Pokemon grid layout

--- src/features/pokemon/pages/PokemonListPage.tsx
+++ src/features/pokemon/pages/PokemonListPage.tsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import { pokemonApi } from "../api/pokemonApi";
-
-import "./PokemonListPage.module";
+import styles from "./PokemonListPage.module.scss";
 
 export default function PokemonListPage() {
   const { data, isLoading } = pokemonApi.useGetPokemonQuery();
@@ -11,17 +10,41 @@ export default function PokemonListPage() {
   }
 
   return (
-    <div>
-      <h1>Pokemon List</h1>
-      {data && (
-        <ul>
-          {data.results.map((pokemon) => (
-            <li key={pokemon.name}>
-              <Link to={`/pokemon/${pokemon.name}`}>{pokemon.name}</Link>
-            </li>
-          ))}
-        </ul>
-      )}
+    <div className={styles.container}>
+      <header>
+        <h1>
+          Pokédex
+          <span className={styles.redDot} />
+        </h1>
+
+        <div className={styles.heroAccent} />
+
+        <h2>Search and explore Pokémon</h2>
+      </header>
+
+      <div className={styles.grid}>
+        {data?.results.map((pokemon, index) => (
+          <Link
+            key={pokemon.name}
+            to={`/pokemon/${pokemon.name}`}
+            className={styles.card}
+          >
+            <img
+              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+                index + 1
+              }.png`}
+              alt={pokemon.name}
+              className={styles.sprite}
+            />
+
+            <span className={styles.id}>
+              N°{String(index + 1).padStart(3, "0")}
+            </span>
+
+            <h3 className={styles.name}>{pokemon.name}</h3>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
