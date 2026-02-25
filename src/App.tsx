80c6cc10b2ab87d534ddaf5a70494fc678d160e1
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2026-02-25 23:04:37 +0530

    feat: integrate PokemonDetailPage into application routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PokemonListPage from "./features/pokemon/pages/PokemonListPage";
+import PokemonDetailPage from "./features/pokemon/pages/PokemonDetailPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<PokemonListPage />} />
-        <Route
-          path="/pokemon/:name"
-          element={<div>Pokemon Details Page</div>}
-        />
+        <Route path="/pokemon/:name" element={<PokemonDetailPage />} />
       </Routes>
     </BrowserRouter>
   );
